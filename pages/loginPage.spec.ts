c94125a8b926be0d1cfe590a85b6e2c1225d5778
anjtoanj <93622823+anjtoanj@users.noreply.github.com>
2024-10-07 09:19:47 +1300

    updated README

--- pages/loginPage.spec.ts
+++ pages/loginPage.spec.ts
@@ -1,6 +1,6 @@
 /* 
 install playwright -> npm install -D @playwright/test@latest
-run the test       ->Navigate to the respective folder -> npx playwright test loginPage.spec.ts
+run the test       ->Navigate to the pages folder -> npx playwright test loginPage.spec.ts
 */
 
 import {test, chromium, expect} from "@playwright/test"
@@ -11,8 +11,8 @@ test(`Test to launch Salesforce page and login with valid credentials`,async () 
     const page = await browserContext.newPage();
     await page.goto("https://login.salesforce.com/");     // launch the application in chrome 
     console.log(page.url);   
-    await page.locator("[name='username']").fill("Thanmayi321@gmail.com");  // Enter credentials
-    await page.locator("[name='pw']").fill("Playwright@2024");
+    await page.locator("[name='username']").fill("****");  // Enter valid username 
+    await page.locator("[name='pw']").fill("****"); // Enter valid password 
     await page.locator("[name='Login']").click();
     await page.waitForTimeout(5000);
     await expect(page).toHaveTitle("Home | Salesforce"); //Verify if the home page is displayed
